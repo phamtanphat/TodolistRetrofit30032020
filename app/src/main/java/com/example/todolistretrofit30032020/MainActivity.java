1cd381fe039212fd470c619fe9a5959ebe87f4ef
phatdroid94 <phatdroid94@gmail.com>
2020-07-08 19:59:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/todolistretrofit30032020/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.todolistretrofit30032020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
